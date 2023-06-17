0ad6d53da4df9196b768376e386e29c5789d71bd
Sebastian <sebastian@bucarion.org>
2023-06-17 17:23:23 -0400

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <h1> Hello </h1>
-      <BrowserRouter>
-       <Routes>
-        {/* Home is for landing page of unauth users */}
-         <Route path="/" element={<Home/>}/>
-       </Routes>
-      </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          {/* Home is for landing page of unauth users */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
+
+  // return (
+  //   <div className="App">
+  //     <h1> Hello </h1>
+  //     <BrowserRouter>
+  //      <Routes>
+  //       {/* Home is for landing page of unauth users */}
+  //        <Route path="/" element={<Home/>}/>
+  //      </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // );
 
 export default App;
