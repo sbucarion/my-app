b04aed8cb97a263e1481bb2e97d2fd116a75d101
Sebastian <sebastian@bucarion.org>
2023-06-17 17:05:15 -0400

    idk2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <h1> Hello </h1>
+      <BrowserRouter basename="/SECDocuViewer">
+       <Routes>
+        {/* Home is for landing page of unauth users */}
+         <Route path="/" element={<Home/>}/>
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { IconButton, Tooltip } from "@material-ui/core";
+import InfoIcon from "@material-ui/icons/Info";
+
+import axios from "axios";
+
+const filingInfo = {
+  "4": "News filing provides updates and announcements about the company.",
+  "8-k":
+    "Alert filing indicates important alerts or notifications related to the company.",
+  "Update": "Update filing contains updates and changes related to the company.",
+};
+
+
+const fetchData = async () => {
+  const response = await axios.get("http://127.0.0.1:8000/getLatestFilings/");
+  const data = response.data;
+
+  return data;
+};
+
+
+const Home = () => {
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchData();
+
+      // TODO
+
+      setJsonData(data);
+    };
+
+    getData();
+  }, []);
+
+
+  return(<h1>hello2</h1>)
+
+  return (
+    <div className="table-container">
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Filing Type</th>
+          <th>Companies</th>
+          <th>Tickers</th>
+          <th>Time</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(jsonData).map((key) => {
+          const item = jsonData[key];
+          return (
+            <tr key={key}>
+              <td>{item.type}</td>
+              <td>{JSON.parse(item.companies)}</td>
+              <td>{item.tickers}</td>
+              <td>{item.time}</td>
+              <td>
+                <a href={item.url}>View Full Filing</a>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+    </div>
+  );
+};
+
+export default Home;
